f1b6ff5add6b637e20688275aaf29edb7676962a
Dimitris <kastrind@gmail.com>
2018-09-13 01:03:49 +0300

    Corrected parallax scrolling. Improved button styling. Added parallax overlay. Introduced slideshow headlines.

--- src/script/ParallaxImg.js
+++ src/script/ParallaxImg.js
@@ -7,18 +7,29 @@ class ParallaxImg {
     this.container = $("div.parallax-container");
     this.img = $("div.parallax-container").find("img");
     this.events();
+    this.correction = 150;
   }
 
   events() {
     $(document).scroll(function () {
       this.doParallaxScrolling();
     }.bind(this));
+
+    //do not do parallax scrolling if image shorter than its container
+    $(window).on("load resize", function() {
+      if (this.img.height() <= this.container.height()) {
+        this.img.css({"top": "50%", "transform": "translateY(-50%)"});
+      }else {
+        this.img.css({"top": "", "transform": ""});
+      }
+    }.bind(this));
   }
 
   doParallaxScrolling() {
-    let inViewAmount = $(document).scrollTop() - this.container.offset().top + $(window).height();
+    let inViewAmount = $(document).scrollTop() - this.container.offset().top + $(window).height() - this.correction;
     let factor = this.img.height() / $(window).height();
     let scrollAmount = inViewAmount * factor;
+    console.log(inViewAmount+" "+scrollAmount);
     if (inViewAmount > 0 && scrollAmount <= this.img.height() - this.container.height()) {
       this.container.find("img").css({"top": -scrollAmount});
     }
