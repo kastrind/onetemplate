15dee31238f339be0f9ee8f5f0c47b6d6442b546
Dimitris <kastrind@gmail.com>
2018-10-23 01:27:19 +0300

    Added header submenu functionality.

--- src/script/NavMenu.js
+++ src/script/NavMenu.js
@@ -6,17 +6,61 @@ class NavMenu {
 		this.siteHeader = $(".site-header");
 		this.menuIcon = $(".site-header .header-nav-btn");
 		this.menuContent = $(".site-header nav.menu");
+		this.menuItem = $(".site-header nav.menu ul li a");
+		this.subMenu = $(".site-header nav.menu ul li > ul");
 		this.events();
 	}
 
 	events() {
+		//toggle the menu on click of the hamburger icon
 		this.menuIcon.click(this.toggleTheMenu.bind(this));
+
+		//on hovering over a menu item, show its submenu, if not already visible
+		this.menuItem.on("mouseenter", function (event) {
+			if ($(event.target).next("ul:first").is(":not(:visible)"))
+			this.toggleSubMenu(event, 'show');
+		}.bind(this));
+
+		//on click of the menu item, show or hide its submenu
+		this.menuItem.on("click", function (event) {
+			let adj_ul = $(event.target).next("ul:first");
+			if (adj_ul.is(":not(:visible)")) {
+				adj_ul.slideDown();
+			}else if (adj_ul.is(":visible")) {
+				adj_ul.slideUp();
+			}
+		}.bind(this));
+
+		//on click of something outside the menu, hide all submenus
+		$(document).on("click", function (event) {
+			if ($(event.target).closest(this.menuContent)[0]==null) {
+				$(this.subMenu).slideUp();
+			}
+		}.bind(this));
+
+		//on hovering away from a menu item, hide its submenu
+		this.subMenu.on("mouseleave", function (event) {
+			this.toggleSubMenu(event, 'hide');
+		}.bind(this));
 	}
 
 	toggleTheMenu() {
-		this.menuContent.toggleClass("menu-visible");
-		this.siteHeader.toggleClass("site-header--is-expanded");
-		this.menuIcon.toggleClass("header-nav-btn-close");
+		if (this.menuIcon.hasClass("header-nav-btn-close")) {
+			this.menuContent.slideUp();
+			this.menuIcon.removeClass("header-nav-btn-close");
+		}else {
+			this.menuContent.slideDown();
+			this.menuIcon.addClass("header-nav-btn-close");
+		}
+	}
+
+	toggleSubMenu(event, status) {
+		if (status=='show') {
+			$(event.target).next("ul:first").slideDown();
+		}else if (status=='hide') {
+			$(event.currentTarget).find("ul").slideUp();
+			$(event.currentTarget).slideUp();
+		}
 	}
 }
 export default NavMenu;
