72c8080250d1cbde3e331738b8d0508661c8b750
Dimitris <kastrind@gmail.com>
2018-10-23 15:08:37 +0300

    Fixed header submenu functionality.

--- src/script/NavMenu.js
+++ src/script/NavMenu.js
@@ -8,6 +8,7 @@ class NavMenu {
 		this.menuContent = $(".site-header nav.menu");
 		this.menuItem = $(".site-header nav.menu ul li a");
 		this.subMenu = $(".site-header nav.menu ul li > ul");
+		this.isMobile = null;
 		this.events();
 	}
 
@@ -15,36 +16,54 @@ class NavMenu {
 		//toggle the menu on click of the hamburger icon
 		this.menuIcon.click(this.toggleTheMenu.bind(this));
 
-		//on hovering over a menu item, show its submenu, if not already visible
-		this.menuItem.on("mouseenter", function (event) {
-			if ($(event.target).next("ul:first").is(":not(:visible)"))
-			this.setSubMenu(event, 'show');
-		}.bind(this));
-
-		//on hovering away from a menu item, hide its submenu
-		this.subMenu.on("mouseleave", function (event) {
-			this.setSubMenu(event, 'hide');
-		}.bind(this));
-
-		//on click of the menu item, show or hide its submenu
-		this.menuItem.on("click", function (event) {
-				setTimeout(function(event) {
-					let adj_ul = $(event.target).next("ul:first");
-					if (adj_ul.is(":not(:visible)")) {
-						adj_ul.slideDown();
-					}else if (adj_ul.is(":visible")) {
-						adj_ul.slideUp();
-					}
-				}.bind(this, event), 300);
-		}.bind(this));
-
-		//on click of something outside the menu, hide all submenus
-		$(document).on("click", function (event) {
-			if ($(event.target).closest(this.menuContent)[0]==null) {
-				$(this.subMenu).slideUp();
+		$(window).on("load resize", function () {
+			let isMobile = this.subMenu.css("position") != 'absolute';
+			let changed = isMobile != this.isMobile;
+			this.isMobile = isMobile;
+			if (changed) {
+				//on mobile view
+				if (this.isMobile) {
+					//on touching a menu item, toggle its submenu visibility
+					this.menuItem.on("touchstart", this.handleTouches.bind(this));
+					this.menuItem.off("mouseenter");
+					this.subMenu.off("mouseleave");
+				//on a larger view
+				}else {
+					this.menuItem.off("touchstart");
+					//on hovering over a menu item, show its submenu, if not already visible
+					this.menuItem.on("mouseenter", this.handleMouseEnter.bind(this));
+					//on hovering away from a menu item, hide its submenu
+					this.subMenu.on("mouseleave", this.handleMouseLeave.bind(this));
+				}
 			}
 		}.bind(this));
 
+	}
+
+	handleTouches(event) {
+		let adj_ul = $(event.target).next("ul:first");
+		if (adj_ul.is(":not(:visible)")) {
+			adj_ul.slideDown();
+		}else if (adj_ul.is(":visible")) {
+			adj_ul.slideUp();
+		}
+	}
+
+	handleMouseEnter(event) {
+		let adj_ul = $(event.target).next("ul:first");
+		if (adj_ul.is(":not(:visible)")) {
+			adj_ul.slideDown();
+		}
+		clearTimeout(this.timer);
+
+		var parent_ul = $(event.target).closest("ul");
+		if (parent_ul[0]) {
+			clearTimeout(parent_ul[0].timer);
+		}
+	}
+
+	handleMouseLeave(event) {
+		event.currentTarget.timer = setTimeout(function(event) {$(event.currentTarget).slideUp();}.bind(this, event), 1000);
 	}
 
 	toggleTheMenu() {
@@ -62,13 +81,5 @@ class NavMenu {
 		}.bind(this));
 	}
 
-	setSubMenu(event, status) {
-		if (status=='show') {
-			$(event.target).next("ul:first").slideDown();
-		}else if (status=='hide') {
-			$(event.currentTarget).find("ul").slideUp();
-			$(event.currentTarget).slideUp();
-		}
-	}
 }
 export default NavMenu;
