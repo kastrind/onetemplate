0c501beca9814eaf3a75ffb0766dfcee1b52080d
Dimitrios Kastrinakis <kastrind@gmail.com>
2019-09-04 21:46:14 +0300

    Fixed back to top, added slideshow slogan animations

--- src/script/BackToTop.js
+++ src/script/BackToTop.js
@@ -6,13 +6,17 @@ class BackToTop {
 
 	constructor(offset, duration, timing_func) {
 		this.backToTopElem = $(".back-to-top");
+		this.backToTopHref = this.backToTopElem.parent().attr('href');
 		this.triggerElem = $("[data-trigger-back-to-top=true]");
+		if (!this.triggerElem.length) {
+			this.triggerElem = $('body');
+		}
 		this.offsetPercentage = offset;
 		this.duration = duration;
 		this.timing_func = timing_func;
+		this.events();
 		this.hideInitially();
 		this.createWaypoints();
-		this.events();
 	}
 
 	hideInitially() {
@@ -26,8 +30,10 @@ class BackToTop {
 			handler: function(direction) {
 				if (direction == "down") {
 					that.backToTopElem.removeClass("back-to-top-hidden");
+					that.backToTopElem.parent().attr('href', that.backToTopHref);
 				}else {
 					that.backToTopElem.addClass("back-to-top-hidden");
+					that.backToTopElem.parent().removeAttr('href');
 				}
 			},
 			offset: that.offsetPercentage
