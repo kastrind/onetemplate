2f0407a2561d2d4ef684d6db8532fda25ac42813
Dimitris <kastrind@gmail.com>
2020-06-16 22:30:43 +0300

    Adjustments to advance to Gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ var addons = [
 /* Task to clean the project from built files */
 gulp.task('clean', function () {
   del('./dist/*');
-  del('./docs/*');
+  return del('./docs/*');
 });
 
 /* Task to copy add-ons and plugins */
@@ -52,8 +52,14 @@ gulp.task('copy-addons', function () {
   .pipe(gulp.dest(addons[addons.length-1].dest));
 });
 
+/* Task to copy assets to dist */
+gulp.task('copy-assets', function () {
+  return gulp.src('src/assets/**/*')
+    .pipe(gulp.dest('dist/assets'));
+});
+
 /* Task to compile less */
-gulp.task('compile-less', ['copy-assets'], function () {
+gulp.task('compile-less', gulp.series('copy-assets', function () {
   return gulp.src('src/less/style.less')
     .pipe(less())
 	  .on('error', function(errorInfo){
@@ -63,25 +69,31 @@ gulp.task('compile-less', ['copy-assets'], function () {
     .pipe(autoprefixer())
     .pipe(gulp.dest('dist/style'))
     .pipe(browserSync.stream());
-});
+}));
+
+gulp.task('create-svg-sprites', gulp.series('copy-assets', function () {
+  return gulp.src('src/assets/img/**/*.svg')
+      .pipe(svgSprite())
+      .pipe(gulp.dest("dist/assets/img/svg"));
+}));
 
 /* Task to minify & bundle css */
-gulp.task('minify-bundle-css', ['compile-less', 'copy-addons', 'create-svg-sprites'], function () {
+gulp.task('minify-bundle-css', gulp.series('compile-less', 'copy-addons', 'create-svg-sprites', function () {
   return gulp.src(sourceStyles)
     .pipe(base64({exclude: [sourceStyles[0]]}))
     .pipe(cleanCSS({rebaseTo : 'dist/style/min'}))
     .pipe(concat('styles.min.css'))
     .pipe(gulp.dest('dist/style/min'))
     .pipe(browserSync.stream());
-});
+}));
 
 /* Task to replace style and script inclusions in HTML with minified, bundled style and script inclusions */
-gulp.task('useref', ['minify-bundle-css'], function () {
+gulp.task('useref', gulp.series('minify-bundle-css', function () {
 return gulp.src('index.html')
     .pipe(replace('src/assets', 'assets'))
     .pipe(useref({ noAssets: true }))
     .pipe(gulp.dest('dist'));
-});
+}));
 
 gulp.task('webpack-prod', function () {
   webpackConfig.devtool = 'source-map';
@@ -89,7 +101,7 @@ gulp.task('webpack-prod', function () {
 });
 
 /* transpile and bundle scripts with webpack */
-gulp.task('transpile-bundle-scripts', ['copy-addons'], function (callback) {
+gulp.task('transpile-bundle-scripts', gulp.series('copy-addons', function (callback) {
 	webpack(webpackConfig, function(err, stats) {
 		if (err) {
 			console.log(err.toString());
@@ -98,45 +110,33 @@ gulp.task('transpile-bundle-scripts', ['copy-addons'], function (callback) {
 		callback();
 	});
   console.log(webpackConfig);
-});
+}));
 
-/* Task to copy assets to dist */
-gulp.task('copy-assets', function () {
-  return gulp.src('src/assets/**/*')
-    .pipe(gulp.dest('dist/assets'));
-});
+gulp.task('build-prod', gulp.series('copy-assets', 'compile-less', 'create-svg-sprites', 'copy-addons', 'minify-bundle-css', 'webpack-prod', 'transpile-bundle-scripts', 'useref'));
 
-gulp.task('create-svg-sprites', ['copy-assets'], function () {
-    return gulp.src('src/assets/img/**/*.svg')
-        .pipe(svgSprite())
-        .pipe(gulp.dest("dist/assets/img/svg"));
-});
+gulp.task('build-dev', gulp.series('compile-less', 'copy-assets', 'copy-addons', 'transpile-bundle-scripts'));
 
-gulp.task('build-prod', ['copy-assets', 'compile-less', 'create-svg-sprites', 'copy-addons', 'minify-bundle-css', 'webpack-prod', 'transpile-bundle-scripts', 'useref']);
-
-gulp.task('build-dev', ['compile-less', 'copy-assets', 'copy-addons', 'transpile-bundle-scripts']);
-
-gulp.task('refresh-prod', ['build-prod'], function() {
+gulp.task('refresh-prod', gulp.series('build-prod', function() {
 	browserSync.reload();
-});
-gulp.task('refresh-dev', ['build-dev'], function() {
+}));
+gulp.task('refresh-dev', gulp.series('build-dev', function() {
 	browserSync.reload();
-});
+}));
 
 /* Configure which files to watch and what tasks to use on file changes */
-gulp.task('watch-prod', ['build-prod'], function() {
+gulp.task('watch-prod', gulp.series('build-prod', function() {
 
   browserSync.init( { server: { baseDir: "./dist" } } );
 
   gulp.watch(['index.html', 'src/script/**/*.js'], function() {
-    gulp.start('refresh-prod');
+    gulp.series('refresh-prod');
   });
 
   gulp.watch('src/less/**/*.less', function() {
-    gulp.start('minify-bundle-css');
+    gulp.series('minify-bundle-css');
   });
 
-});
+}));
 
 /* Configure which files to watch and what tasks to use on file changes */
 gulp.task('watch-dev', function() {
@@ -148,30 +148,30 @@ gulp.task('watch-dev', function() {
   });
 
   gulp.watch('src/script/**/*.js', function() {
-    gulp.start('refresh-dev');
+    gulp.series('refresh-dev');
   });
 
   gulp.watch('src/less/**/*.less', function() {
-    gulp.start('compile-less');
+    gulp.series('compile-less');
   });
 
-  gulp.start('copy-assets');
+  gulp.series('copy-assets');
   gulp.watch('src/less/paths.less', function() {
-    gulp.start('copy-assets');
+    gulp.series('copy-assets');
   });
 
-  gulp.start('create-svg-sprites');
+  gulp.series('create-svg-sprites');
   gulp.watch('src/assets/img/**/*.svg', function() {
-    gulp.start('create-svg-sprites');
+    gulp.series('create-svg-sprites');
   });
 
 });
 
 /* Task to create folder to be published on github */
-gulp.task('create-github-page', ['build-prod'], function () {
+gulp.task('create-github-page', gulp.series('build-prod', function () {
   return gulp.src('dist/**/*')
     .pipe(gulp.dest('docs'));
-});
+}));
 
 /* Define the default task and add the watch task to it */
-gulp.task('default', ['watch-dev']);
+gulp.task('default', gulp.series('watch-dev'));
